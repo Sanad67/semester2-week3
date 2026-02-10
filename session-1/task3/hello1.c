ad664191025410ac59a14b5ba8e336b44519c335
Sanad Dodokh <kgzn0742@leeds.ac.uk>
2026-02-10 10:43:06 +0000

    Semester 2 week 3 lesson 1

--- session-1/task3/hello1.c
+++ session-1/task3/hello1.c
@@ -1,15 +1,15 @@
 
 /*
- * Hello world program 
+ * Hello world program
  * - compilation errors
  */
 
 #include <stdio.h>
 
-int main( void ) {
-    
-    printf("Hello\n")
+int main(void)
+{
 
-    return 0;    
-}	
+    printf("Hello\n");
 
+    return 0;
+}
